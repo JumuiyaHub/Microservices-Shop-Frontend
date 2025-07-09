76d487e7219944cd7dcb4d3312ed05eaa73b6f48
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2025-07-09 07:46:02 +0300

    refactor(order-service): simplify HTTP POST request for order submission

--- src/app/services/order/order.service.ts
+++ src/app/services/order/order.service.ts
@@ -12,13 +12,15 @@ export class OrderService {
   constructor(private httpClient: HttpClient) {
   }
 
-  orderProduct(order: Order): Observable<string> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      }),
-      responseType: 'text' as 'json'
-    };
-    return this.httpClient.post<string>('http://localhost:9000/api/order', order, httpOptions);
+  orderProduct(order: Order): Observable<any> {
+    // const httpOptions = {
+    //   headers: new HttpHeaders({
+    //     'Content-Type': 'application/json'
+    //   }),
+    //   responseType: 'text' as 'json'
+    // };
+    const url = 'http://localhost:9000/api/order';
+    return this.httpClient.post<any>(url, order);
+    // return this.httpClient.post<any>('http://localhost:9000/api/order', order, httpOptions);
   }
 }
